eeded01220abaa095903d71f9bbbce6d0389d0f8
Johannes Winkelmann <jw@smts.ch>
2017-10-20 14:35:40 -0700

    loop in setup() if there’s an init problem

--- examples/ess/ess.ino
+++ examples/ess/ess.ino
@@ -11,42 +11,39 @@ void setup()
       Serial.print("Error while initializing sensors: ");
       Serial.print(ess.getError());
       Serial.print("\n");
+      while (1) { // loop forever
+        delay(1000);
+      }
   }
 }
 
 void loop() {
-  if (ess.isInitialized()) {
-    float temp, rh, tvoc = -1;
+  float temp, rh, tvoc = -1;
 
-    if (ess.measureRHT() != 0) {
-      Serial.print("Error while measuring RHT: ");
-      Serial.print(ess.getError());
-      Serial.print("\n");
-    } else {
-      temp = ess.getTemperature();
-      rh = ess.getHumidity();
-    }
-
-    if (ess.measureIAQ() != 0) {
-      Serial.print("Error while measuring IAQ: ");
-      Serial.print(ess.getError());
-      Serial.print("\n");
-    } else {
-      tvoc = ess.getTVOC();
-    }
-
-    Serial.print(temp);
-    Serial.print(" ");
-    Serial.print(rh);
-    Serial.print(" ");
-    Serial.print(tvoc);
-    Serial.print(" ");
+  if (ess.measureRHT() != 0) {
+    Serial.print("Error while measuring RHT: ");
+    Serial.print(ess.getError());
     Serial.print("\n");
-
-    delay(ess.remainingWaitTimeMS());
   } else {
-    while (1) { // loop forever, error message was already printed in setup()
-      delay(1000);
-    }
+    temp = ess.getTemperature();
+    rh = ess.getHumidity();
   }
+
+  if (ess.measureIAQ() != 0) {
+    Serial.print("Error while measuring IAQ: ");
+    Serial.print(ess.getError());
+    Serial.print("\n");
+  } else {
+    tvoc = ess.getTVOC();
+  }
+
+  Serial.print(temp);
+  Serial.print(" ");
+  Serial.print(rh);
+  Serial.print(" ");
+  Serial.print(tvoc);
+  Serial.print(" ");
+  Serial.print("\n");
+
+  delay(ess.remainingWaitTimeMS());
 }

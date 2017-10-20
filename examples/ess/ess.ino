07db4a3737c89eaf81271f7a950bdbfa5cd4bc9e
Johannes Winkelmann <jw@smts.ch>
2017-10-20 13:23:23 -0700

    check for initialization, don’t loop fast on err

--- examples/ess/ess.ino
+++ examples/ess/ess.ino
@@ -14,36 +14,44 @@ void setup()
   }
 }
 
-float temp, rh, tvoc = -1;
+void loop() {
+  if (ess.isInitialized()) {
+    float temp, rh, tvoc = -1;
 
-void loop()
-{
-  temp, rh, tvoc = -1;
+    if (ess.measureRHT() != 0) {
+      Serial.print("Error while measuring RHT: ");
+      Serial.print(ess.getError());
+      Serial.print("\n");
+    } else {
+      temp = ess.getTemperature();
+      rh = ess.getHumidity();
+    }
 
-  if (ess.measureRHT() != 0) {
-    Serial.print("Error while measuring RHT: ");
-    Serial.print(ess.getError());
+    if (ess.measureIAQ() != 0) {
+      Serial.print("Error while measuring IAQ: ");
+      Serial.print(ess.getError());
+      Serial.print("\n");
+    } else {
+      tvoc = ess.getTVOC();
+    }
+
+    Serial.print(temp);
+    Serial.print(" ");
+    Serial.print(rh);
+    Serial.print(" ");
+    Serial.print(tvoc);
+    Serial.print(" ");
     Serial.print("\n");
-  } else {
-    temp = ess.getTemperature();
-    rh = ess.getHumidity();
-  }
 
-  if (ess.measureIAQ() != 0) {
-    Serial.print("Error while measuring IAQ: ");
-    Serial.print(ess.getError());
+    delay(ess.remainingWaitTimeMS());
+  } else {
+    Serial.print("ESS not initialized. Trying to reinitialize");
     Serial.print("\n");
-  } else {
-    tvoc = ess.getTVOC();
+    if (ess.initSensors() != 0) {
+        Serial.print("Error while initializing sensors: ");
+        Serial.print(ess.getError());
+        Serial.print("\n");
+    }
+    delay(1000);
   }
-
-  Serial.print(temp);
-  Serial.print(" ");
-  Serial.print(rh);
-  Serial.print(" ");
-  Serial.print(tvoc);
-  Serial.print(" ");
-  Serial.print("\n");
-
-  delay(ess.remainingWaitTimeMS());
 }
